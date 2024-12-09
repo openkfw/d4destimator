c3fb8022233b6c8464d917403707626f55f768f2
jzakotnik <jure@zakotnik.de>
2024-12-09 11:24:17 +0100

    updated readme and cost formatting

--- src/components/CostSummary.jsx
+++ src/components/CostSummary.jsx
@@ -3,19 +3,33 @@ import { Box, Typography, Paper } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 
 const CostSummary = ({ totalInitial, totalRun, totalCostOfOwnership }) => {
+  //do some formatting
+  const formattedTotalInitial = new Intl.NumberFormat("de-DE", {
+    style: "currency",
+    currency: "EUR",
+  }).format(totalInitial);
+  const formattedTotalRun = new Intl.NumberFormat("de-DE", {
+    style: "currency",
+    currency: "EUR",
+  }).format(totalRun);
+  const formattedTotalCostOfOwnership = new Intl.NumberFormat("de-DE", {
+    style: "currency",
+    currency: "EUR",
+  }).format(totalCostOfOwnership);
+
   return (
     <Box p={2} component={Paper}>
       <Grid container spacing={2} direction="row" alignItems="flex-start">
         <Grid size={6}>
           <Typography align="left">Total Initial Cost:</Typography>
         </Grid>
-        <Grid size={6}>{totalInitial} €</Grid>
+        <Grid size={6}>{formattedTotalInitial} </Grid>
       </Grid>
       <Grid container item spacing={2} direction="row" alignItems="flex-start">
         <Grid size={6}>
           <Typography align="left">Total Run Cost per Year:</Typography>
         </Grid>
-        <Grid size={6}>{totalRun} €</Grid>
+        <Grid size={6}>{formattedTotalRun} </Grid>
       </Grid>
       <Grid container item spacing={2} direction="row" alignItems="flex-start">
         <Grid size={6}>
@@ -26,7 +40,7 @@ const CostSummary = ({ totalInitial, totalRun, totalCostOfOwnership }) => {
 
         <Grid size={6}>
           <Typography sx={{ fontWeight: "bold" }}>
-            {totalCostOfOwnership} €
+            {formattedTotalCostOfOwnership}
           </Typography>
         </Grid>
       </Grid>
