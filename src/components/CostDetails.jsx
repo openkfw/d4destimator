bb8975cfc793ea80c00eae4acd8ca2a011cc6b1f
fretchen <fred.jendrzejewski@gmail.com>
2024-12-10 08:50:51 +0100

    Update the table to include main cost categories.

--- src/components/CostDetails.jsx
+++ src/components/CostDetails.jsx
@@ -13,7 +13,16 @@ import {
 
 const CostDetails = ({ config }) => {
   console.log("Received a new engine result", config);
+  const categories = {};
+  Object.values(config.costs).forEach((cost) => {
+    if (!categories[cost.category]) {
+      categories[cost.category] = [];
+    }
+    categories[cost.category].push(cost);
+  });
 
+  const sortedCategories = Object.keys(categories).sort();
+  console.log("Sorted categories", sortedCategories);
   return (
     <div>
       <TableContainer>
@@ -32,27 +41,35 @@ const CostDetails = ({ config }) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {Object.keys(config.costs).map((key) => (
-              <TableRow key={key}>
-                <TableCell>{config.costs[key].label}</TableCell>
-                <TableCell>
-                  <Typography variant="body">
-                    {new Intl.NumberFormat("de-DE", {
-                      style: "currency",
-                      currency: config.constants.dailyrate.currency,
-                    }).format(config.costs[key].value)}
-                  </Typography>
-                </TableCell>
-                <TableCell>
+            {sortedCategories.map((category) => (
+              <React.Fragment key={category}>
+                <TableRow>
+                  <TableCell colSpan={3}>
+                    <Typography variant="h6">{category}</Typography>
+                  </TableCell>
+                </TableRow>
+                {categories[category].map((cost) => (
+                  <TableRow key={cost.label}>
+                    <TableCell>{cost.label}</TableCell>
+                    <TableCell>
+                      <Typography variant="body">
+                        {new Intl.NumberFormat("de-DE", {
+                          style: "currency",
+                          currency: config.constants.dailyrate.currency,
+                        }).format(cost.value)}
+                      </Typography>
+                    </TableCell><TableCell>
                   {" "}
                   {new Intl.NumberFormat("de-DE", {
                     style: "currency",
                     currency: config.constants.dailyrate.currency,
                   }).format(
-                    config.costs[key].value * config.constants.runcosts.value,
+                    cost.value * config.constants.runcosts.value,
                   )}
                 </TableCell>
-              </TableRow>
+                  </TableRow>
+                ))}
+              </React.Fragment>
             ))}
           </TableBody>
         </Table>
