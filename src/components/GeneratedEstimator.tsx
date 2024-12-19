22d946efce4316a09b08a0f24f574bbab1e93834
fretchen <fred.jendrzejewski@gmail.com>
2024-12-19 17:20:24 +0100

    No more anys

--- src/components/GeneratedEstimator.tsx
+++ src/components/GeneratedEstimator.tsx
@@ -5,25 +5,29 @@ import {
   Select,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import runEngine from "../utils/runEngine";
+import { EstimatorConfig } from "../types/estimatorConfigType";
 
-const GeneratedEstimator = ({
+
+interface GeneratedConstantsProps {
+  estimatorConfig: EstimatorConfig;
+  setCalculation: Dispatch<SetStateAction<EstimatorConfig>>;
+}
+
+const GeneratedEstimator: React.FC<GeneratedConstantsProps> = ({
   estimatorConfig,
   setCalculation,
-}: {
-  estimatorConfig: any;
-  setCalculation: any;
 }) => {
   const parameters = estimatorConfig.parameters;
   console.log("Rendering input parameters", parameters);
-  const handleChange = (key: any, value: any) => {
+  const handleChange = (key: string, value: string) => {
     console.log("Changing input", key, value); //e.g. users, 50+
 
     //for that field, set all selected options to false and toggle the selected to true
     const newEstimatorConfig = { ...estimatorConfig };
 
-    newEstimatorConfig.parameters[key].values.forEach((v: any) => {
+    newEstimatorConfig.parameters[key].values.forEach((v) => {
       console.log("Changing this field", v);
       v.selected = false;
       if (v.inputFactor === value) {
@@ -43,7 +47,7 @@ const GeneratedEstimator = ({
         {estimatorConfig.flavour} parameters
       </Typography>
       <div>
-        {Object.entries(parameters).map(([field, data]: [string, any]) => (
+        {Object.entries(parameters).map(([field, data]) => (
           <FormControl key={field} fullWidth style={{ marginBottom: "16px" }}>
             <InputLabel id="demo-simple-select-standard-label">
               {data.label}
@@ -52,10 +56,10 @@ const GeneratedEstimator = ({
               sx={{ fontPalette: "black" }}
               labelId={`${field}-label`}
               label={data.label}
-              value={data.values.filter((d: any) => d.selected)[0].inputFactor}
+              value={data.values.filter((d) => d.selected)[0].inputFactor}
               onChange={(e) => handleChange(field, e.target.value)}
             >
-              {data.values.map((option: any, index: any) => (
+              {data.values.map((option, index) => (
                 <MenuItem key={index} value={option.inputFactor}>
                   {option.inputFactor}
                 </MenuItem>
