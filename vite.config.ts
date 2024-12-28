bc8d478ef4d029e21f7482034d150f1e9fb0a7a7
fretchen <fred.jendrzejewski@gmail.com>
2024-12-28 14:48:16 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/d4destimator/",
   plugins: [react()],
   build: {
     outDir: "build",
