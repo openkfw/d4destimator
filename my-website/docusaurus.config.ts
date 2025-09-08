4d4aa3fa09c3a0eef175f51318f8544ce04118fe
fretchen <polka.99gefiedert@icloud.com>
2025-09-08 08:38:59 +0200

    Clean it up with proper header etc

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'D4DEstimator',
+  tagline: 'Simple budget estimates in D4D',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://openkfw.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/d4destimator/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'openkfw', // Usually your GitHub org/user name.
+  projectName: 'd4destimator', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -39,10 +39,12 @@ const config: Config = {
   presets: [
     [
       'classic',
+      
       {
         theme: {
           customCss: './src/css/custom.css',
         },
+        blog: false, // Disable blog completely
       } satisfies Preset.Options,
     ],
   ],
@@ -51,53 +53,23 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'D4DEstimator',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'D4DEstimator Logo',
         src: 'img/logo.svg',
       },
+      items: [
+          {
+            href: 'https://github.com/openkfw/d4destimator',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
     },
     footer: {
       style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      links: [],
+      copyright: `Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
---
+++ my-website/src/pages/App.tsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from 'react';
+
+import './App.css';
+
+import CostOverview from '../components/CostOverview';
+
+import GeneratedEstimator from '../components/GeneratedEstimator';
+import runEngine from '../utils/runEngine';
+
+import estimatorConfig from '../assets/dashboard_config.json';
+import { EstimatorConfig } from '../types/estimatorConfigType';
+
+import { Box, Container, Paper} from '@mui/material';
+import Grid from '@mui/material/Grid2';
+import GeneratedConstants from '../components/GeneratedConstants';
+
+const estimatorConfigTyped: EstimatorConfig = estimatorConfig;
+
+function App() {
+  const [calculation, setCalculation] = useState(estimatorConfigTyped);
+  useEffect(() => {
+    const result = runEngine(estimatorConfigTyped);
+    setCalculation(result);
+
+    console.log('Running engine:', result);
+  }, []);
+
+  return (
+    <div className="App">
+      <Box
+        sx={{
+          minHeight: '100vh',
+          background:
+            ' linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(10,48,5,1) 35%, rgba(19,4,136,1) 100%)',
+          color: 'white',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+
+
+        {/* Content */}
+        <Container sx={{ flex: 1, py: 4 }}>
+          <Grid
+            container
+            spacing={4}
+            sx={{
+              height: '100%',
+            }}
+          >
+            {/* Left Side */}
+            <Grid size={{ xs: 12, md: 4 }}>
+              <Paper
+                elevation={3}
+                sx={{
+                  p: 3,
+                  borderRadius: 2,
+                  backgroundColor: 'rgba(255, 255, 255, 1.0)',
+                }}
+              >
+                <div>
+                  <GeneratedEstimator
+                    estimatorConfig={calculation}
+                    setCalculation={setCalculation}
+                  />
+                </div>
+              </Paper>
+              <Paper
+                elevation={3}
+                sx={{
+                  p: 3,
+                  my: 2,
+                  borderRadius: 2,
+                  backgroundColor: 'rgba(255, 255, 255, 1.0)',
+                }}
+              >
+                <div>
+                  <GeneratedConstants
+                    estimatorConfig={calculation}
+                    setCalculation={setCalculation}
+                  />
+                </div>
+              </Paper>
+            </Grid>
+
+            {/* Right Side */}
+            <Grid size={{ xs: 12, md: 8 }}>
+              <Paper
+                elevation={3}
+                sx={{
+                  p: 3,
+                  borderRadius: 2,
+                  backgroundColor: 'rgba(255, 255, 255, 0.)',
+                }}
+              >
+                <CostOverview config={calculation} />
+              </Paper>
+            </Grid>
+          </Grid>
+        </Container>
+
+      </Box>
+    </div>
+  );
+}
+
+export default App;
